22021637acca615e7bae4bb2f82bfa27ff80a1df
ATHARV SAKOLKAR <atharvsakolkar2002@gmail.com>
2026-05-30 09:30:05 +0530

    main.tf file and jenkins file was updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,3 @@
-// tf file for eks cluster
-
 terraform {
   required_version = ">= 1.3"
   required_providers {
@@ -11,14 +9,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 data "aws_vpc" "default" { // Fetch default VPC
   default = true
 }
 variable "cluster_name" { // Variable for cluster name
   type    = string
-  default = "my-cluster"
+  default = "jenkins-cluster"
 
 }
 
@@ -113,5 +111,4 @@ output "cluster_name" { // Output for cluster name
 output "cluster_endpoint" {    //  Output for cluster endpoint
   value = aws_eks_cluster.mycluster.endpoint
 }
-
 
